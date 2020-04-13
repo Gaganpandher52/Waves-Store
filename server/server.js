8d6716940cd9a33d58ce5602be32725aebb98962
gaganpandher52 <gaganpandher52@gmail.com>
2020-04-13 15:05:06 -0500

    implemented postman and post request

--- server/server.js
+++ server/server.js
@@ -13,14 +13,23 @@ app.use(bodyParser.urlencoded({entended:true}));
 app.use(bodyParser.json()); 
 app.use(cookieParser());
 
-const { User } = require('./models/user')
+const { User } = require('./models/user');
 
 //==================================
 //              USERS
 //==================================
 
-app.post('./api/users/register',(req,res)=>{
+app.post('/api/users/register',(req,res)=>{
   res.status(200)
+  const user = new User(req.body);
+
+  user.save((err,doc)=>{
+    if(err) return res.json({success:false,err});
+    res.status(200).json({
+      success:true,
+      userdata: doc
+    })
+  })
 })
 
 
