aa676365ec88b5f9b2cb79ba2cff2c02d8d5c0b9
gaganpandher52 <gaganpandher52@gmail.com>
2020-04-15 20:25:00 -0500

    added logic for modifying the password

--- server/models/user.js
+++ server/models/user.js
@@ -44,16 +44,21 @@ const userSchema = mongoose.Schema({
 
 userSchema.pre('save',function(next){
   var user = this
-  bcrypt.genSalt(SALT,function(err,salt){
-    if(err) return next(err)
 
-    bcrypt.hash(user.password,salt,function(err,hash){
-      if(err) return next(err);
+  if(user.isModified('password')){
+    bcrypt.genSalt(SALT,function(err,salt){
+      if(err) return next(err)
+      
+      bcrypt.hash(user.password,salt,function(err,hash){
+        if(err) return next(err);
         user.password = hash;
         next();
+      })
+      
     })
-
-  })
+  } else{
+    next();
+  }
 
 })
 
