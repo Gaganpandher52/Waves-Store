eddefbda8fdc46b1211413cd7fc3f52b38b7f64a
gaganpandher52 <gaganpandher52@gmail.com>
2020-04-21 12:33:18 -0500

    fixed a typo when entering wrong email and pass

--- server/server.js
+++ server/server.js
@@ -35,7 +35,7 @@ app.post('/api/users/register',(req,res)=>{
 app.post('/api/users/login',(req,res)=>{
 
   User.findOne({'email':req.body.email},(err,user)=>{
-    if(!user) return res.json({loginSuccess:fasle,message:'Auth failes, email not found'})
+    if(!user) return res.json({loginSuccess:false,message:'Auth failes, email not found'})
 
     user.comparePassword(req.body.password,(err,isMatch)=>{
       if(!isMatch) return res.json({loginSuccess:'false',message:'Wrong Password'})
