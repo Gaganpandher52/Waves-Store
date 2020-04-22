b8295cafdbff7b4681f8be722737f157007edc1f
gaganpandher52 <gaganpandher52@gmail.com>
2020-04-22 15:10:40 -0500

    customized the response

--- server/server.js
+++ server/server.js
@@ -25,7 +25,14 @@ const { auth } = require('./middleware/auth')
 
 app.get('/api/users/auth',auth,(req,res)=>{
   res.status(200).json({
-    user: req.user
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    cart: req.user.cart,
+    history: req.user.history
 
   })
 
