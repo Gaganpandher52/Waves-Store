d89abe5c27007ec7daf81452fbbbe9b4b3a5d6f8
gaganpandher52 <gaganpandher52@gmail.com>
2020-05-12 16:14:29 -0500

    added product model

--- server/models/product.js
+++ server/models/product.js
@@ -1,7 +1,28 @@
 const mongoose = require('mongoose');
 
 const productSchema = mongoose.Schema({
+  name:{
+    required: true,
+    type: String,
+    unique: 1,
+    maxlength:100,
+  },
+  description:{
+    required: true,
+    type: String,
+    maxlength:100000,
+  },
+  price:{
+    required: true,
+    type: Number,
+    maxlength:255,
+  },
+  brand:{
+    
+  }
   
 });
 
-const Product = mongoose.model('Product',productSchema) 
+const Product = mongoose.model('Product',productSchema);
+
+module.exports = { Product }
